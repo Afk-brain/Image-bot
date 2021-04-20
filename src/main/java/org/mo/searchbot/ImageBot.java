f5ad097154577a7e0b3a3908f1bdb4944e524423
Code Master <makovskyi.alexander@gmail.com>
2021-04-20 12:30:11 +0300

    Clean ImageBot class

--- src/main/java/org/mo/searchbot/ImageBot.java
+++ src/main/java/org/mo/searchbot/ImageBot.java
@@ -2,13 +2,11 @@ package org.mo.searchbot;
 
 import org.mo.searchbot.utils.BotCommand;
 import org.mo.searchbot.utils.GoogleImageService;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
 import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageMedia;
 import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
 import org.telegram.telegrambots.meta.api.objects.InputFile;
 import org.telegram.telegrambots.meta.api.objects.Message;
-import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.media.InputMediaPhoto;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
@@ -19,7 +17,6 @@ import java.util.*;
 public class ImageBot extends CommandBot {
 
     private final GoogleImageService imageService = new GoogleImageService();
-    private final static int ATTEMPTS = 5;
     private Map<Integer, List<String>> storage = new HashMap<>();
     private int id = 0;
 
@@ -35,38 +32,10 @@ public class ImageBot extends CommandBot {
                 .replyToMessageId(message.getMessageId())
                 .replyMarkup(getKeyboard(links, 0, 0))
                 .build();
-        int c = 0;
-        while(c++ < ATTEMPTS) {
-            try {
-                execute(sendPhoto);
-                return;
-            } catch (TelegramApiException e) {
-                e.printStackTrace();
-            }
-            sendPhoto.setPhoto(new InputFile(chooseLink(links)));
-        }
-        replyText(message, "Fuck off");
-    }
-
-    private String chooseLink(List<String> links) {//TODO rework
-        Random random = new Random();
-        return links.get(random.nextInt(Math.min(9, links.size())));
-    }
-
-    @BotCommand("\\/help")
-    public void help(Message message) {
-        replyText(message, "Big Floppa is gay");//TODO add description
-    }
-
-    @BotCommand("\\/version")
-    public void version(Message message) {
-        Properties properties = new Properties();
         try {
-            properties.load(getClass().getResourceAsStream("/gis.prefs"));
-            replyText(message, properties.getProperty("version"));
-        } catch (IOException e) {
+            execute(sendPhoto);
+        } catch (TelegramApiException e) {
             e.printStackTrace();
-            replyText(message, "Nice dick, bro");
         }
     }
 
