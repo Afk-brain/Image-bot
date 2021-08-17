842f6f61d5c4ea2101a453add335e9321fec1912
Code Master <makovskyi.alexander@gmail.com>
2021-08-17 12:32:50 +0300

    Add multiline demotivation

--- src/main/java/org/mo/searchbot/utils/Demotivator.java
+++ src/main/java/org/mo/searchbot/utils/Demotivator.java
@@ -1,5 +1,6 @@
 package org.mo.searchbot.utils;
 
+import org.apache.commons.text.WordUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -9,6 +10,7 @@ import java.awt.image.BufferedImage;
 public class Demotivator {
 
     private static Logger log = LoggerFactory.getLogger(Demotivator.class);
+    private static double averageWidth = 0.5;
 
     private BufferedImage src;
     private int innerBlack;
@@ -16,25 +18,31 @@ public class Demotivator {
     private int blackTop;
     private int blackSide;
     private int textHeight;
+    private int textDistance;
 
     public Demotivator(BufferedImage image) {
         log.info("Defining image parameters");
         src = image;
         innerBlack = Math.max(src.getHeight(), src.getWidth()) / 200;
         innerWhite = innerBlack;
-        blackTop = src.getHeight() / 10;
+        blackTop = Math.max(src.getHeight() / 10, src.getWidth() / 10);
         blackSide = src.getWidth() / 10;
-        textHeight = src.getHeight() / 8;
+        textHeight = Math.min(src.getHeight() / 8, src.getWidth() / 8);
+        textDistance = textHeight / 4;
     }
 
     public BufferedImage demotivate(String text) {
-        log.info("Drawing!!!");
+        log.info("start");
+        int maxSymbols = (src.getWidth() + innerWhite + innerBlack + blackSide) / (int)(textHeight * averageWidth);
+        text = WordUtils.wrap(text, maxSymbols, null, true);
+        String[] lines = text.split("\n");
+        log.info(text + "\n" + lines.length);
         BufferedImage newImage = new BufferedImage(src.getWidth() + 2 * (innerBlack + innerWhite + blackSide),
-                src.getHeight() + 2 * (innerBlack + innerWhite)  + 3 * blackTop + textHeight, src.getType());
+                src.getHeight() + 2 * (innerBlack + innerWhite)  + 2 * blackTop + textHeight * lines.length + textDistance * (lines.length - 1), src.getType());
         Graphics graphics = newImage.createGraphics();
         drawInnerBorders(graphics);
         graphics.drawImage(src, blackSide + innerBlack + innerWhite, blackTop + innerBlack + innerWhite, null);
-        drawText(graphics, text);
+        drawText(graphics, lines);
         graphics.dispose();
         return newImage;
     }
@@ -47,12 +55,15 @@ public class Demotivator {
         graphics.fillRect(blackSide + innerWhite, blackTop + innerWhite, src.getWidth() + 2 * innerBlack, src.getHeight() + 2 * innerBlack);
     }
 
-    private void drawText(Graphics graphics, String text) {
+    private void drawText(Graphics graphics, String[] lines) {
         graphics.setColor(Color.WHITE);
         graphics.setFont(new Font("TimesRoman", Font.PLAIN, textHeight));
-        int textWidth = graphics.getFontMetrics().stringWidth(text);
-        int borders = 2 * (innerBlack + innerWhite + blackTop);
-        graphics.drawString(text, (src.getWidth() - textWidth) / 2 + blackSide, src.getHeight() + borders + blackTop);
+        for(int i = 0;i < lines.length;i++) {
+            String line = lines[i];
+            int textWidth = graphics.getFontMetrics().stringWidth(line);
+            int borders = 2 * (innerBlack + innerWhite + blackTop / 2);
+            graphics.drawString(line, (src.getWidth() - textWidth) / 2 + blackSide, src.getHeight() + borders + blackTop + i * (textHeight + textDistance));
+        }
     }
 
 
